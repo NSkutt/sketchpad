078f967c1acc71de52e59c3fdad9fe0e790a59fd
NSkutt <nskutt@protonmail.com>
2022-03-25 17:58:45 -0500

    etchasketch area phase 1 complete, mouseenter adds a class that changes the box to black, can rerun function for new box

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let squares = 16;
-
+let boxes;
 function pad(){
+	container.replaceChildren(); //When function runs again, empty container of old boxes
 	//container.style.backgroundColor = "red";
 	for (i=1; i<=squares*squares; i++){
 		window[`square${i}`] = document.createElement('div');
@@ -9,7 +10,18 @@ function pad(){
 		//console.log(window[`square${i}`]);
 		window[`square${i}`].style.minWidth = `${container.clientWidth / squares }px`;
 		window[`square${i}`].style.minHeight  = `${container.clientHeight / squares }px`;
+		//window[`square${i}`].style.backgroundColor = `#${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}${Math.floor(Math.random()*10)}`;
 		container.append(window[`square${i}`]);
 	};
+	boxes = [...document.querySelectorAll('.boxes')];
+	return boxes;
 }
 pad();
+
+function color(){
+	//console.log(this);
+	//this.style.backgroundColor = '';
+	this.classList.add('hovered');
+}
+
+boxes.forEach(box => {box.addEventListener('mouseenter', color)})
